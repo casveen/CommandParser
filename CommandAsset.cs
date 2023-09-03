1ff9c08b218e1996506a5ee79f5d208a6a0b2f10
Christian Alexander Holm Sveen <casveen@student.matnat.uio.no>
2023-09-03 13:54:59 +0200

    major cleanup

--- CommandAsset.cs
+++ CommandAsset.cs
@@ -1,31 +1,7 @@
-using UnityEngine;
-using System.Collections;
 using Warudo.Core.Attributes;
-using Warudo.Core.Graphs;
 using Warudo.Core.Scenes;
-using Warudo.Plugins.Core.Assets;
-using Warudo.Plugins.Core.Assets.Character;
-using Warudo.Plugins.Core.Assets.Prop;
-using Warudo.Core.Data.Models;
-using Animancer;
 using Cysharp.Threading.Tasks;
-
-using Warudo.Core.Server;
-
-using Warudo.Plugins.Core.Assets.Utility;
-using Warudo.Plugins.Core.Assets.Cinematography;
-
-using System;
-//using Cysharp.Threading.Tasks;
-using Warudo.Core;
 using Warudo.Core.Data;
-using RootMotion.Dynamics;
-using Warudo.Core.Utils;
-using Warudo.Core.Localization;
-using Warudo.Plugins.Core;
-using Warudo.Plugins.Core.Utils;
-using Warudo.Plugins.Interactions.Mixins;
-using Object = UnityEngine.Object;
 using System.Collections.Generic;
 
 
@@ -38,10 +14,10 @@ namespace Playground {
         [SectionAttribute("Commands",0)]
         [DataInput]
         public Command[] Commands = new Command[] {};
-
+ 
         [DataInput]
         [Label("Case-sensitive commands")]
-        public bool caseSensitive = false;
+        public bool CaseSensitive = false;
 
                 [DataInput]
         [Label("Command Prefix")]
@@ -55,156 +31,136 @@ namespace Playground {
         [SectionAttribute("Groups",1)]
         [DataInput]
         [Label("Groups")]
-        public UserGroup[] groups; 
-        public Dictionary<string, UserGroup> groupDict = new Dictionary<string, UserGroup>();
+        public UserGroup[] Groups; 
+        public Dictionary<string, UserGroup> GroupDict = new Dictionary<string, UserGroup>();
 
         [DataInput]
         [Label("Use Twitch Subsgriber Group")]
-        public bool useTwitchSubscriberGroup = false;
+        public bool UseTwitchSubscriberGroup = false;
         
         [DataInput]
         [Label("Use Twitch VIP Group")]
-        public bool useTwitchVIPGroup = false;
+        public bool UseTwitchVIPGroup = false;
 
         [DataInput]
         [Label("Use Twitch Moderator Group")]
-        public bool useTwitchModeratorGroup = false;
+        public bool UseTwitchModeratorGroup = false;
 
         [DataInput]
         [Label("Use Twitch Broadcaster")]
-        public bool useTwitchBroadcaster = false;
+        public bool UseTwitchBroadcaster = false;
 
         [DataInput]
         [Label("Case-sensitive Names")]
-        public bool caseSensitiveNames = false;
+        public bool CaseSensitiveNames = false;
 
-        
 
-        //CLASSES
+
         public class Command : StructuredData<CommandAsset>, ICollapsibleStructuredData {
-
             [DataInput]
             [Label("Command Name")]
-            public string command = null;
+            public string Name = null;
 
 
             [DataInput]
             [Label("Accesible To Everyone")]
-            public bool accesibleToEveryone = true;
+            public bool AccesibleToEveryone = true;
 
-            public bool accesibleToEveryoneGetter() => accesibleToEveryone;
-            
+            protected bool AccesibleToEveryoneGetter() => AccesibleToEveryone;
+            protected async UniTask<AutoCompleteList> GetUserGroups() {
+                return AutoCompleteList.Single(LoadListAsync(Parent?.Groups));
+            }
+            protected System.Collections.Generic.IEnumerable<Warudo.Core.Data.AutoCompleteEntry> LoadListAsync(UserGroup[] toLoad) {
+                foreach (UserGroup ug in toLoad) {
+                    yield return new AutoCompleteEntry {
+                        label = ug.Name,
+                        value = ug.Name
+                    };
+                }
+            }
             [DataInput]
-            [HiddenIf(nameof(accesibleToEveryoneGetter))]
-            //[AutoCompleteResourceAttributenameof(UserGroup), null]
+            [HiddenIf(nameof(AccesibleToEveryoneGetter))]
             [Label("Accesible To Groups")]
             [AutoComplete(nameof(GetUserGroups), true)]
-            public string[] AccesibleTo; 
-            //public UserGroup[] AccesibleToGroups;
+            public string[] AccesibleTo; //has to be string, so as to play nice with AutoCompleteList
 
-            protected bool HideSubscriber() { return accesibleToEveryone || (!Parent.useTwitchSubscriberGroup); }
-            protected bool HideVIP() { return accesibleToEveryone || (!Parent.useTwitchVIPGroup); }
-            protected bool HideModerator() { return accesibleToEveryone || (!Parent.useTwitchModeratorGroup); }
-            protected bool HideBroadcaster() { return accesibleToEveryone || (!Parent.useTwitchBroadcaster); }
-            protected bool HideArguments() { return !hasArguments; }
+            protected bool HideSubscriber() { return AccesibleToEveryone || (!Parent.UseTwitchSubscriberGroup); }
+            protected bool HideVIP() { return AccesibleToEveryone || (!Parent.UseTwitchVIPGroup); }
+            protected bool HideModerator() { return AccesibleToEveryone || (!Parent.UseTwitchModeratorGroup); }
+            protected bool HideBroadcaster() { return AccesibleToEveryone || (!Parent.UseTwitchBroadcaster); }
+            protected bool HideArguments() { return !HasArguments; }
 
             [DataInput]
             [HiddenIf(nameof(HideSubscriber))]
             [Label("Accesible To Subscribers")]
-            public bool accesibleToSubscribers = true; 
+            public bool AccesibleToSubscribers = true; 
 
             [DataInput]
             [HiddenIf(nameof(HideVIP))]
             [Label("Accesible To VIPs")]
-            public bool accesibleToVIPs = true; 
+            public bool AccesibleToVIPs = true; 
 
             [DataInput]
             [HiddenIf(nameof(HideModerator))]
             [Label("Accesible To Moderators")]
-            public bool accesibleToModerators = true; 
+            public bool AccesibleToModerators = true; 
 
             [DataInput]
             [HiddenIf(nameof(HideBroadcaster))]
             [Label("Accesible To Broadcaster")]
-            public bool accesibleToBroadcaster = true; 
+            public bool AccesibleToBroadcaster = true; 
 
             [DataInput]
             [Label("Has Arguments")]
-            public bool hasArguments = false; 
+            public bool HasArguments = false; 
 
             [DataInput]
             [HiddenIf(nameof(HideArguments))]
             [Label("Arguments")]
-            public Argument[] arguments;
+            public Argument[] Arguments;
             public class Argument : StructuredData, ICollapsibleStructuredData {
                 [DataInput]
                 [Label("Argument Name")]
-                public string ArgumentName;
+                public string Name;
 
                 [DataInput]
                 [Label("Argument Type")]
-                public CommandAsset.ParseType ArgumentType;
+                public ParseType Type;
 
                 public string GetHeader() {
-                    return ArgumentName??"<Argument name not set>";
+                    return Name??"<Argument name not set>";
                 }
             }
-
-
-
-
-
-
-            public async UniTask<AutoCompleteList> GetUserGroups() {
-                return AutoCompleteList.Single(loadListAsync(Parent?.groups));
-            }
-
-            public System.Collections.Generic.IEnumerable<Warudo.Core.Data.AutoCompleteEntry> loadListAsync(UserGroup[] toLoad) {
-                foreach (UserGroup ug in toLoad) {
-                    yield return new AutoCompleteEntry {
-                        label = ug.name,
-                        value = ug.name
-                    };
-                }
-            }
-            
-            //TRIGGER CHANGE IN ALL NODES!!!
 
             public string GetHeader() {
-                return command??"<Command name not set>";
+                return Name??"<Command name not set>";
             }
-
-            /*public class Argument : StructuredData, ICollapsibleStructuredData {
-                [DataInput]
-                public string argumentName;
-                public string GetHeader() {
-                    return argumentName??"<Argument name not set>";
-                }
-            }*/
         }
 
         
         public class UserGroup : StructuredData, ICollapsibleStructuredData {
             [DataInput]
             [Label("Name of group")]
-            public string name = "";
+            public string Name = "";
 
             [DataInput]
             [Label("Name of members")]
-            public string[] members;
+            public string[] Members;
 
             public string GetHeader() {
-                return name??"<Group name not set>";
+                return Name??"<Group name not set>";
             }
         }
 
         protected override void OnCreate() {
             base.OnCreate();
             SetActive(true);
-            Watch(nameof(groups), () => {
-                groupDict.Clear();// XXX extremely ineffecient...
-                foreach (UserGroup Group in groups) {
-                    groupDict.Add(Group.name, Group);
+
+            //tie name of group to actual group. Needed because of the AutoCompleteList restriction. An extremely inefficient implementation
+            Watch(nameof(Groups), () => {
+                GroupDict.Clear();
+                foreach (UserGroup Group in Groups) {
+                    GroupDict.Add(Group.Name, Group);
                 }
                 Broadcast();
             });
--- CommandParser.cs
+++ CommandParser.cs
@@ -1,32 +1,9 @@
-using UnityEngine;
-using System.Collections;
-using Warudo.Core.Attributes;
 using Warudo.Core.Graphs;
-using Warudo.Core.Scenes;
-using Warudo.Plugins.Core.Assets;
-using Warudo.Plugins.Core.Assets.Character;
-using Warudo.Plugins.Core.Assets.Prop;
-using Warudo.Core.Data.Models;
-using Animancer;
+using Warudo.Core.Attributes;
+using Warudo.Core.Data;
 using System.Collections.Generic;
 using System.Linq;
-using Warudo.Core.Server;
-
-using Warudo.Plugins.Core.Assets.Utility;
-using Warudo.Plugins.Core.Assets.Cinematography;
-
 using System;
-//using Cysharp.Threading.Tasks;
-using Warudo.Core;
-using Warudo.Core.Data;
-using RootMotion.Dynamics;
-using Warudo.Core.Utils;
-using Warudo.Core.Localization;
-using Warudo.Plugins.Core;
-using Warudo.Plugins.Core.Utils;
-using Warudo.Plugins.Interactions.Mixins;
-using Object = UnityEngine.Object;
-
 
 namespace Playground {
     //[NodeType(),
@@ -37,89 +14,57 @@ namespace Playground {
     public class CommandParserNode : Node {
         protected Dictionary<string, string> ParseDict = new Dictionary<string, string>();
 
-        protected bool hideSubscriber() { return !(Commander?.useTwitchSubscriberGroup??false); }
-        protected bool hideModerator() { return !(Commander?.useTwitchModeratorGroup??false); }
-        protected bool hideBroadcaster() { return !(Commander?.useTwitchBroadcaster??false); }
-        protected bool hideVIP() { return !(Commander?.useTwitchVIPGroup??false); }
+        protected bool HideSubscriber() { return !(Commander?.UseTwitchSubscriberGroup??false); }
+        protected bool HideModerator() { return !(Commander?.UseTwitchModeratorGroup??false); }
+        protected bool HideBroadcaster() { return !(Commander?.UseTwitchBroadcaster??false); }
+        protected bool HideVIP() { return !(Commander?.UseTwitchVIPGroup??false); }
 
         [DataInput]
-        public string username = "";
+        public string Username = "";
         [DataInput]
-        public string message = "";
+        public string Message = "";
         [DataInput]
-        [HiddenIf(nameof(hideSubscriber))]
-        public bool isSubscriber = false;
+        [HiddenIf(nameof(HideSubscriber))]
+        public bool IsSubscriber = false;
 
         [DataInput]
-        [HiddenIf(nameof(hideBroadcaster))]
-        public bool isBroadcaster = false;
+        [HiddenIf(nameof(HideBroadcaster))]
+        public bool IsBroadcaster = false;
         [DataInput]
-        [HiddenIf(nameof(hideModerator))]
-        public bool isModerator = false;
+        [HiddenIf(nameof(HideModerator))]
+        public bool IsModerator = false;
         [DataInput]
-        [HiddenIf(nameof(hideVIP))]
-        public bool isVIP = false;
-
-        [DataInput]
-        [IntegerSlider(0,3,1)]
-        public int visibleArguments = 1; 
+        [HiddenIf(nameof(HideVIP))]
+        public bool IsVIP = false;
 
         [DataInput]
         public CommandAsset Commander;
 
-        //private Dictionary<string, Func<int>> IntValues = new Dictionary<string, Func<int>>();
-        //private Dictionary<string, Func<float>> FloatValues = new Dictionary<string, Func<float>>();
-        //private Dictionary<string, Func<string>> StringValues = new Dictionary<string, Func<string>>();
-        
         [FlowInput]
         public Continuation Enter() {
-            //parse, and invoke appropriate flow.
+            //parse command, check if user can use the command, find arguments and invoke appropriate flow.
+            //note that the actual parsing happens "in" the outputports, not here.
+            StringComparison comparisonType = Commander.CaseSensitive switch {
+                true  => StringComparison.InvariantCultureIgnoreCase, 
+                false => StringComparison.InvariantCulture
+            };
             //check if a command
-            Debug.Log("ENTER");
-            if((Commander != null) && (message?.StartsWith(Commander.CommandPrefix)??false)) {
-                Debug.Log("Found a command!");
-                //parse  
-                string[] split = message.Substring(1).Split(Commander.ArgumentSeparator);
-                Debug.Log("HERES THE SPLIT");
-                foreach (string s  in split) {
-                    Debug.Log(s);
-                }
-                Debug.Log("---");
-
-                string command = (string) split.GetValue(0);
-                
-
+            if((Commander != null) && (Message?.StartsWith(Commander.CommandPrefix)??false)) {
+                string[] split = Message.Substring(1).Split(Commander.ArgumentSeparator);
+                string parsedCommand = (string) split.GetValue(0);
                 //find the correct command
-                foreach (var Command in Commander.Commands) { //int i=0; i<Commander.Commands.Length; i++) {
-                    string CommandName = Command.command;
-                    Debug.Log("Checking command " + CommandName);
-                    //var Command = Commander.Commands[i];
-                    if ( String.Equals(CommandName, command, StringComparison.InvariantCultureIgnoreCase)) {
-                        Debug.Log("thats the one");
-                        if (UserIsVerifiedToUse(Command)) {
-                            Debug.Log("verified!");
-                            ///Command found, user is able to run it 
-                            //Parse arguments
-                            Debug.Log("parsing args");
-                            var ArgAndText = Command.arguments.Zip(split.Skip(1), (a,t) => (a,t));
-                            foreach (var (Argument, Text) in ArgAndText) {
-                                //var Argument = Command.arguments[j-1];
-                                Debug.Log(Argument);
-                                string PortName = Command.command + ":" + Argument.ArgumentName;
-                                if (ParseDict.ContainsKey(PortName)) {
-                                    ParseDict[PortName] = Text;
-                                } else {
-                                    ParseDict.Add(PortName,Text);
-                                }
-                                Debug.Log("Registered " + Text + " at output " + CommandName + ":" + Argument.ArgumentName);
+                foreach (CommandAsset.Command command in Commander.Commands) {
+                    if (String.Equals(command.Name, parsedCommand, comparisonType)) {
+                        if (UserIsVerifiedToUse(command)) {
+                            //command found, user is able to run it
+                            //assign strings to be parsed to each argument(via a dictionary)
+                            foreach (var (argument, text) in command.Arguments.Zip(split.Skip(1), (a,t) => (a,t))) {
+                                string portName = command.Name + ":" + argument.Name;
+                                if (ParseDict.ContainsKey(portName)) { ParseDict[portName] = text;} 
+                                else {ParseDict.Add(portName,text);}
                             } 
-                            Debug.Log("INVOKE " + CommandName);
-                                //argumentText[j-1] = split[j];
-                            InvokeFlow(CommandName); 
-                        } else {
-                            Debug.Log("not verified!");
-                        }
-                        //FlowOutputPort port = FlowOutputPortCollection.GetPort(CommandName);
+                            InvokeFlow(command.Name); 
+                        } 
                     }
                 }
             }
@@ -137,27 +82,26 @@ namespace Playground {
                 SetupExitPorts();
                 SetupOutputPorts();
             });
-
-            Watch(nameof(visibleArguments), () => {
-                SetupOutputPorts();
-            });
-            
             SetupExitPorts();
             SetupOutputPorts();
         } 
  
-        public bool UserIsVerifiedToUse(CommandAsset.Command command) {
-            if (command.accesibleToEveryone || 
-                (command.accesibleToBroadcaster && isBroadcaster) ||
-                (command.accesibleToVIPs && isVIP) ||
-                (command.accesibleToModerators && isModerator) ||
-                (command.accesibleToSubscribers && isSubscriber)) {
+        protected bool UserIsVerifiedToUse(CommandAsset.Command command) {
+            StringComparison comparisonType = Commander.CaseSensitiveNames switch {
+                true  => StringComparison.InvariantCultureIgnoreCase, 
+                false => StringComparison.InvariantCulture
+            };
+            if (command.AccesibleToEveryone || 
+                (command.AccesibleToBroadcaster && IsBroadcaster) ||
+                (command.AccesibleToVIPs && IsVIP) ||
+                (command.AccesibleToModerators && IsModerator) ||
+                (command.AccesibleToSubscribers && IsSubscriber)) {
                     return true;
             } 
-            foreach (string GroupName in command.AccesibleTo) {
-                var Group = command.Parent.groupDict[GroupName];
-                foreach (string member in Group.members) {
-                    if ( member == username) {
+            //check if user is in one of the groups who can run the command
+            foreach (string groupName in command.AccesibleTo) {
+                foreach (string member in command.Parent.GroupDict[groupName].Members) {
+                    if (string.Equals(Username, member, comparisonType)) {
                         return true;
                     }
                 }
@@ -167,99 +111,60 @@ namespace Playground {
 
         
         public void SetupExitPorts() {
-            var ExitCount = Commander?.Commands?.Length ?? 0;
-
+            int exitCount = Commander?.Commands?.Length ?? 0;
             FlowOutputPortCollection.GetPorts().Clear();
-            for (var i = 1; i <= ExitCount; i++) {
-                var commandName = Commander.Commands[i-1].command;
+            for (int i = 0; i < exitCount; i++) {
+                string commandName = Commander.Commands[i].Name;
                 AddFlowOutputPort(commandName, new FlowOutputProperties {
-                    label = commandName, //"EXIT".Localized() + " " + i
+                    label = commandName,
                     description = "The flow output triggered on recieving a " + commandName + " command"
                 });
             }
-
             Broadcast();
         }
 
         public void SetupOutputPorts() {
-            Debug.Log("Setting up output ports");
             DataOutputPortCollection.GetPorts().Clear();
             if (Commander != null) {
-                Debug.Log("Setting up output ports - cleared");
-                foreach (CommandAsset.Command Command in Commander.Commands) {
-                    
-                    string CommandName = Command.command;
-                    Debug.Log("Setting up output ports - command " + CommandName);
-                    int i=0; 
-                    foreach (CommandAsset.Command.Argument Argument in Command.arguments) {
-                        string ArgumentName = Argument.ArgumentName;
-                         Debug.Log("Setting up output ports - command " + CommandName + " with argument " + ArgumentName);
-                        Type ArgumentType = typeof(int);
-                        switch(Argument.ArgumentType) {
-                            case CommandAsset.ParseType.FLOAT:
-                                ArgumentType=typeof(float);
-                                
-                                Debug.Log(CommandName + ":" + ArgumentName);
-                                AddDataOutputPort(
-                                    CommandName + ":" + ArgumentName,
-                                    ArgumentType,
+                foreach (CommandAsset.Command command in Commander.Commands) {
+                    string commandName = command.Name;
+                    foreach (CommandAsset.Command.Argument argument in command.Arguments) {
+                        string argumentName = argument.Name;
+                        string portName = commandName + ":" + argumentName;
+                        AddDataOutputPort(
+                            portName,
+                            argument.Type switch {
+                                CommandAsset.ParseType.FLOAT =>  typeof(float),
+                                CommandAsset.ParseType.INT =>    typeof(int),
+                                CommandAsset.ParseType.STRING => typeof(string)
+                            },
+                            argument.Type switch {
+                                CommandAsset.ParseType.FLOAT => 
                                     () => {
                                         float parsed = 0.0f;
-                                        bool success = float.TryParse(ParseDict[CommandName + ":" + ArgumentName], out parsed);
+                                        bool success = float.TryParse(ParseDict[portName], out parsed);
                                         return success?parsed:0;
                                     },
-                                    new DataOutputProperties {
-                                        label = CommandName + ":" + ArgumentName,
-                                        description = "The parsed output from parsing the " + ArgumentName + " argument in a " + CommandName + " command"
-                                    }
-                                );
-                                break;
-                            case CommandAsset.ParseType.INT: //XXX return how to parse an int!!!
-                                ArgumentType=typeof(int);
-                                Debug.Log(CommandName + ":" + ArgumentName);
-                                AddDataOutputPort(
-                                    CommandName + ":" + ArgumentName,
-                                    ArgumentType,
+                                CommandAsset.ParseType.INT =>
                                     () => {
                                         int parsed = 0;
-                                        bool success = int.TryParse(ParseDict[CommandName + ":" + ArgumentName], out parsed);
+                                        bool success = int.TryParse(ParseDict[portName], out parsed);
                                         return success?parsed:0;
                                     },
-                                    new DataOutputProperties {
-                                        label = CommandName + ":" + ArgumentName,
-                                        description = "The parsed output from parsing the " + ArgumentName + " argument in a " + CommandName + " command"
-                                    }
-                                );
-                                break;
-                            case CommandAsset.ParseType.STRING:
-                                ArgumentType=typeof(string);
-                                Debug.Log(CommandName + ":" + ArgumentName);
-                                AddDataOutputPort(
-                                    CommandName + ":" + ArgumentName,
-                                    ArgumentType,
+                                CommandAsset.ParseType.STRING => 
                                     () => {
-                                        Debug.Log("OUTOUT GETTING " + CommandName + ":" + ArgumentName);
-                                        Debug.Log(ParseDict.ContainsKey(CommandName + ":" + ArgumentName));
-                                        Debug.Log(ParseDict);
-                                        return ParseDict.ContainsKey(CommandName + ":" + ArgumentName)?ParseDict[CommandName + ":" + ArgumentName]:"";
-                                    },
-                                    new DataOutputProperties {
-                                        label = CommandName + ":" + ArgumentName,
-                                        description = "The parsed output from parsing the " + ArgumentName + " argument in a " + CommandName + " command"
-                                    }
-                                );
-                                break;
-                            
-                        } 
-                        i++; 
+                                        return ParseDict.ContainsKey(portName)?ParseDict[portName]:"";
+                                    } 
+                            },
+                            new DataOutputProperties {
+                                label = portName,
+                                description = "The parsed output from parsing the " + argumentName + " argument in a " + commandName + " command"
+                            }
+                        );
                     }
                 }
             }
             Broadcast();
         }
     }
-
-    /*heartContainer = Plugin.ModHost.Assets.Load<GameObject>("Assets/Feline's Heart Containers/Heart.prefab");
-GameObject go = Object.Instantiate(heartContainer);
-go.transform.parent = this.GameObject.transform;*/
 }
